abd742c74b4937fa30e869daa3d7c7fbed1f7302
Koushik2824 <95124356+Koushik2824@users.noreply.github.com>
2023-06-22 18:03:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import tensorflow as tf
+st.set_option('deprecation.showfileUploaderEncoding',False)
+class_names = ['Fake','Real']
+@st.cache_data#so that we dont need to load the model each time
+def load_model():
+  model=tf.keras.models.load_model('/Users/koushikmukka/Desktop/my_model2.hdf5')
+  return model
+
+
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write(
+         """
+         # AI-Real Image Classification
+         """
+         )
+
+file = st.file_uploader("Please upload image to check if its real or ai generated", type=["jpg", "png"])
+
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+def import_and_predict(image_data, model):
+        size = (32,32)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_reshape = img[np.newaxis,...]#model is trained on 4dimensions,this line adds new dimension
+        prediction = model.predict(img_reshape)        
+        return prediction
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)#display 
+    predictions = import_and_predict(image, model)
+    score = tf.nn.sigmoid(predictions[0])
+    st.write(predictions)
+    st.write(score)
+    print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
